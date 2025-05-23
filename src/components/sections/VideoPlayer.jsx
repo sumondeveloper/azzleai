77068ec04b9eb11be9768a165d43991368ccdfb7
Rakibul Islam Sumon <rakibuldevs@gmail.com>
2025-05-23 22:46:28 +0600

    fix gradient essue

--- src/components/sections/VideoPlayer.jsx
+++ src/components/sections/VideoPlayer.jsx
@@ -7,8 +7,8 @@ const VideoPlayer = () => {
   return (
     <section className="bg-white lg:py-24 py-15 relative ">
       {/* Gradient Background */}
-      <div className="orange-gradient-1 absolute top-[400px] h-[400px] w-[250px] -rotate-[-9.022deg] rounded-[400px] z-20 hidden lg:block"></div>
-      <div className="orange-gradient-1 absolute top-[300px] h-[600px] w-[250px] -rotate-[-9.022deg] rounded-[400px] z-20 hidden lg:block"></div>
+      <div className="orange-gradient-1 absolute top-[400px] h-[400px] w-[250px] -rotate-[-9.022deg] rounded-[400px] z-2 hidden lg:block"></div>
+      <div className="orange-gradient-1 absolute top-[300px] h-[600px] w-[250px] -rotate-[-9.022deg] rounded-[400px] z-2 hidden lg:block"></div>
 
       {/* Main Content */}
       <div className="mx-auto max-w-[1500px] sm:px-5 px-2 ">
