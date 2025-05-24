963153bcff9e386a55477fadce9f547af2e26875
Rakibul Islam Sumon <rakibuldevs@gmail.com>
2025-05-24 22:26:00 +0600

    add faq

--- src/pages/Faq.jsx
+++ src/pages/Faq.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef } from "react";
 import PageNavbar from "../components/shared/PageNavbar";
 import { faqsData } from "../utils/constants";
 import PlusIcon from "../assets/icons/plus.svg";
