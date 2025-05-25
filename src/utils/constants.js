94679b3c96f728d74b6fc2d54bcd1bdc957fe64c
Rakibul Islam Sumon <rakibuldevs@gmail.com>
2025-05-25 21:20:45 +0600

    fix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1220,14 +1220,3 @@ export const fadeUpVariant = {
     },
   }),
 };
-export const fadeUp = {
-  hidden: { opacity: 0, y: 40 },
-  show: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      duration: 0.6,
-      ease: "easeOut",
-    },
-  },
-};
