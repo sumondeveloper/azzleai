c949c58f76c6e40fabf74f653f4f3bc03d80043e
Rakibul Islam Sumon <rakibuldevs@gmail.com>
2025-05-25 23:36:17 +0600

    add animation to the portfolio details

--- src/components/sections/AudienceSection.jsx
+++ src/components/sections/AudienceSection.jsx
@@ -9,48 +9,43 @@ const AudienceSection = () => {
     <section className="bg-white py-5 text-primary overflow-hidden">
       <MaxWidthWrapper>
         {/* Top Section */}
-        <div className="flex flex-col justify-between sm:flex-row items-center lg:gap-25 sm:gap-8">
-          <AnimatonVariants
-            animation="fade-left"
-            delay={0.7}
-            duration={0.7}
-            className="mt-10 lg:w-[100%] h-auto sm:w-[50%]"
-          >
+        <AnimatonVariants
+          animation="fade-left"
+          delay={0.7}
+          duration={0.7}
+          className="mt-10 lg:w-[100%] h-auto sm:w-[50%]"
+        >
+          <div className="flex flex-col justify-between sm:flex-row items-center lg:gap-25 sm:gap-8">
             <img src={ContentImageOne} alt="" className="w-full h-auto" />
-          </AnimatonVariants>
 
-          <AnimatonVariants
-            animation="fade-right"
-            delay={0.5}
-            duration={0.7}
-            className=""
-          >
-            <div>
-              <h2 className="text-[36px] font-secondary font-semibold leading-[40px] tracking-tight lg:text-[80px] lg:leading-[80px] lg:tracking-normal sm:font-bold sm:leading-10 mt-10 mb-5">
-                Accessible to a wider audience
-              </h2>
-              <p className="font-primary text-base lg:text-lg mt-4">
-                Advanced AI capabilities accessible to a broader audience,
-                including small & medium-sized businesses and individuals who
-                may not have the resources or expertise to develop.
-              </p>
-              <p className="font-primary text-base lg:text-lg mt-4">
-                Advanced AI capabilities accessible to a broader audience,
-                including small & medium-sized businesses and individuals who
-                may not have the resources or expertise to develop.
-              </p>
-            </div>
-          </AnimatonVariants>
-        </div>
+            <AnimatonVariants
+              animation="fade-right"
+              delay={0.5}
+              duration={0.7}
+              className=""
+            >
+              <div>
+                <h2 className="text-[36px] font-secondary font-semibold leading-[40px] tracking-tight lg:text-[80px] lg:leading-[80px] lg:tracking-normal sm:font-bold sm:leading-10 mt-10 mb-5">
+                  Accessible to a wider audience
+                </h2>
+                <p className="font-primary text-base lg:text-lg mt-4">
+                  Advanced AI capabilities accessible to a broader audience,
+                  including small & medium-sized businesses and individuals who
+                  may not have the resources or expertise to develop.
+                </p>
+                <p className="font-primary text-base lg:text-lg mt-4">
+                  Advanced AI capabilities accessible to a broader audience,
+                  including small & medium-sized businesses and individuals who
+                  may not have the resources or expertise to develop.
+                </p>
+              </div>
+            </AnimatonVariants>
+          </div>
+        </AnimatonVariants>
 
         {/* Bottom Section */}
         <div className="mt-15 flex flex-col sm:flex-row sm:justify-between items-center sm:mt-20 sm:gap-8">
-          <AnimatonVariants
-            animation="fade-right"
-            delay={0.5}
-            duration={0.7}
-            className=""
-          >
+          <AnimatonVariants animation="fade-right" delay={0.5} duration={0.7}>
             <div>
               <h2 className="text-[36px] font-secondary font-semibold leading-[40px] tracking-tight lg:text-[80px] lg:leading-[80px] lg:tracking-normal sm:font-bold sm:leading-10 ">
                 Providing quick deploy solutions
